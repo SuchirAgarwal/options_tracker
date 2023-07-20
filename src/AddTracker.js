f8c4930a09fef4e1253bc39cfd1a33965739c2b5
SuchirAgarwal <44748663+SuchirAgarwal@users.noreply.github.com>
2023-07-20 01:09:40 +0000

    add tracker form, no logic yet

--- src/AddTracker.js
+++ src/AddTracker.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react'
 
 export class AddTracker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      symbol: '',
+      price: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit() {
+
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
   render() {
     return (
-      <div>AddTracker</div>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label for='symbol'>Option Symbol</label>
+          <input 
+            type='text' 
+            name='symbol' 
+            id='symbol' 
+            onChange={this.handleChange}
+            value={this.state.symbol} 
+            required
+          />
+        </div>
+        <div>
+          <label for='price'>Purchase/Sell Price</label>
+          <input 
+            type='number' 
+            name='price' 
+            id='price' 
+            onChange={this.handleChange}
+            value={this.state.price} 
+            required
+          />
+        </div>
+        <div>
+          <input value='Submit' type='submit' />
+        </div>
+      </form>
     )
   }
 }
